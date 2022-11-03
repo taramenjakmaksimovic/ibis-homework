4fe5062a91db2a7597d2bc8fff614a8d89cfb698
taramenjakmaksimovic <taramm19@gmail.com>
2022-11-03 23:52:25 +0100

    Initial commit

---
+++ clicks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Clicks(models.Model):
+    click_id = models.CharField(max_length=128)
+    timestamp = models.IntegerField()
+    type = models.CharField(max_length=128)
+    campaign = models.IntegerField()
+    banner = models.IntegerField()
+    content_unit = models.IntegerField()
+    network = models.IntegerField()
+    browser = models.IntegerField()
+    operating_system = models.IntegerField()
+    country = models.IntegerField()
+    state = models.IntegerField()
+    city = models.IntegerField()
